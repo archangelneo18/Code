1999214daeaedbc93c5b0b8df241e42e6cd73773
ArchAngelNeo <archangelneo18@gmail.com>
2022-08-25 13:37:43 -0400

    adding code 100 days to repo

---
+++ 100DaysOfCode/bandnamegenerator.py
@@ -0,0 +1,7 @@
+from ssl import CHANNEL_BINDING_TYPES
+
+
+print("Welcome to Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pets name?\n")
+print("Your Band name could be " + city + " " + pet)
---
+++ 100DaysOfCode/tip.py
@@ -0,0 +1,12 @@
+# num_char = len(input("What is your name"))
+# print("your name has " + num_char+ " characters.")
+
+# print(type(num_char))
+
+
+# a = 123
+# print(type(a))
+
+
+
+print(3 * 3 + 3 / 3 -3)
