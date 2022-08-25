68f16aa3429d044d25759048f7d3833f7a68ea94
ArchAngelNeo <archangelneo18@gmail.com>
2022-08-25 16:20:14 -0400

    updated

--- 100DaysOfCode/tip.py
+++ 100DaysOfCode/tip.py
@@ -1,12 +1,14 @@
-# num_char = len(input("What is your name"))
-# print("your name has " + num_char+ " characters.")
-
-# print(type(num_char))
-
-
-# a = 123
-# print(type(a))
+#if the bill was $150.00, split between 5 people, with 12% tip.
+#Each person should pay (150.00 / 5) * 1.12
+from selectors import EVENT_READ
 
 
+print("Welcome to tip calculator!")
 
-print(3 * 3 + 3 / 3 -3)
+bill = float(input("What is the total bill? $"))
+tip = int(input("What percentage tip would you like to give? "))
+split = int(input("How many people to split the bill? "))
+bill_with_tipy = tip / 100 * bill + bill
+
+
+print(bill_with_tipy)
